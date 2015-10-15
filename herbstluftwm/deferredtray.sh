01fbe0b28f25ad3df4676100ce2677ce9b415194
Serge Tkatchouk <sp1j3t@gmail.com>
2015-10-16 07:35:22 +0800

    Separate systray script.

---
+++ herbstluftwm/deferredtray.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pgrep 'trayer' > /dev/null || trayer --edge top --align center --widthtype request --heighttype pixel --height 14 --transparent true --alpha 0 --tint 0x101010 --align right --distancefrom right --distance 310 &
+pgrep 'nm-applet' > /dev/null || nm-applet
+pgrep 'redshift' > /dev/null || redshift-gtk
