21024d26af8d3f3742c9ae7f70fdf2e708c45850
Serge Tkatchouk <s.tkachuk@baikal.ttk.ru>
2015-11-11 09:14:54 +0800

    Detect if Ruby is actually installed.

--- zsh/options/ruby.sh
+++ zsh/options/ruby.sh
@@ -1,2 +1,5 @@
-export PATH="$(ruby -e 'print Gem.user_dir')/bin:$PATH"
-export GEM_HOME=$(ruby -e 'print Gem.user_dir')
+if [[ /usr/bin/env ruby -v > /dev/null 2>&1 ]]; then
+	export PATH="$(ruby -e 'print Gem.user_dir')/bin:$PATH"
+	export GEM_HOME=$(ruby -e 'print Gem.user_dir')
+fi
+
