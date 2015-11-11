c19be72d6bfe8d92ba87262b58df3dc27133dc61
Serge Tkatchouk <s.tkachuk@baikal.ttk.ru>
2015-11-11 09:45:57 +0800

    I can't into shell script.

--- zsh/options/ruby.sh
+++ zsh/options/ruby.sh
@@ -1,4 +1,5 @@
-if [[ /usr/bin/env ruby -v > /dev/null 2>&1 ]]; then
+if which ruby > /dev/null
+	then
 	export PATH="$(ruby -e 'print Gem.user_dir')/bin:$PATH"
 	export GEM_HOME=$(ruby -e 'print Gem.user_dir')
 fi
