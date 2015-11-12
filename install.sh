c6b5ddab58d298bb4d3a5ab301871451f420a268
Serge Tkatchouk <sp1j3t@gmail.com>
2015-11-12 21:31:47 +0800

    Changed Bash to POSIX Shell, added ZSH/tmux/HLWM installation.

--- install.sh
+++ install.sh
@@ -1,5 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
-[ -z $HOME/.vimrc ] && ln -s ./vim/.vimrc $HOME/.vimrc
-[ -z $HOME/.vim ] && ln -s ./vim/.vimrc $HOME/.vim
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/Vundle.vim
+echo "Installing ZSH stuff..."
+  [ -z $HOME/.zshrc ] && ln -s ./.zshrc $HOME/.zshrc
+  [ -z $HOME/.zsh ] && ln -s ./zsh $HOME/.zsh
+  [ -z $HOME/.zsh/plugins/oh-my-zsh ] && git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.zsh/plugins/oh-my-zsh
+  [ -z $HOME/.zsh/plugins/autosuggestions ] && git clone https://github.com/tarruda/zsh-autosuggestions $HOME/.zsh/plugins/autosuggestions
+
+echo "Installing HLWM symlinks..."
+  [ -z $HOME/.config/herbstluftwm ] && ln -s ./herbstluftwm $HOME/.config/herbstluftwm
+
+echo "Installing tmux configs..."
+  [ -z $HOME/.tmux.conf ] && ln -s ./tmux/tmux.conf $HOME/.tmux.conf
+  [ -z $HOME/.tmuxline.conf ] && ln -s ./tmux/tmuxline.conf $HOME/.tmuxline.conf
+
+echo "Installing vim symlinks and cloning Vundle..."
+  [ -z $HOME/.vimrc ] && ln -s ./vim/.vimrc $HOME/.vimrc
+  [ -z $HOME/.vim ] && ln -s ./vim/.vimrc $HOME/.vim
+  git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/Vundle.vim
+
