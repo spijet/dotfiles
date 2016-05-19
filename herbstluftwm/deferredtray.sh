5a933070c46cf8886558a54d5d08aaee02b758bc
Serge Tkatchouk <sp1j3t@gmail.com>
2016-05-19 17:25:38 +0800

    Fixed trayer. I have to use "trayer-srg" on Gentoo, so tray script now has check for which version of trayer is installed.

--- herbstluftwm/deferredtray.sh
+++ herbstluftwm/deferredtray.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-sleep 3
-pgrep 'trayer' > /dev/null || trayer --edge top --align center --widthtype request --heighttype pixel --height 14 --transparent true --alpha 0 --tint 0x101010 --align right --distancefrom right --distance 310 &
-pgrep 'nm-applet' > /dev/null || nm-applet &
-pgrep 'redshift' > /dev/null || redshift-gtk &
+for command in trayer trayer-srg
+do
+    which $command >/dev/null 2>&1 && TRAYER_COMMAND=`which $command`
+done
+if [[ -z $TRAYER_COMMAND ]]; then
+    exit 0
+else
+    sleep 3
+    pgrep 'trayer' > /dev/null || $TRAYER_COMMAND --edge top --align center --widthtype request --heighttype pixel --height 14 --transparent true --alpha 0 --tint 0x101010 --align right --distancefrom right --distance 310 &
+    pgrep 'nm-applet' > /dev/null || nm-applet &
+    pgrep 'redshift' > /dev/null || redshift-gtk &
+fi
