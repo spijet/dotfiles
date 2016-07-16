9a0f43d7ae0393a1d6b6ad6e1e3d02aaa029d516
Serge Tkatchouk <sp1j3t@gmail.com>
2016-07-17 00:44:02 +0800

    Some HLWM fixes and new LED backlight helper.

--- herbstluftwm/light.sh
+++ herbstluftwm/light.sh
@@ -1,13 +1,49 @@
 #!/bin/bash
+set -x
 
+# Read current PWM frequency and set delta and cap
+setmode() {
+    PWMFREQ=$(cat /tmp/pwmfreq)
+    if [ "${PWMFREQ}" = "125" ]; then
+        BL_DELTA="15"
+        BL_CAP="127"
+    else
+        BL_DELTA="25"
+        BL_CAP="937"
+    fi
+}
+
+up() {
+    CUR_BRIGHT=$(</sys/class/backlight/intel_backlight/brightness)
+    NEW_BRIGHT=$(( CUR_BRIGHT + BL_DELTA ))
+
+    # Avoid overbrights so there's no need to decrease brightness all the way from 900%
+    [ "${NEW_BRIGHT}" -gt "${BL_CAP}" ] && NEW_BRIGHT="${BL_CAP}"
+
+    echo "${NEW_BRIGHT}" | sudo tee /sys/class/backlight/intel_backlight/brightness
+}
+
+down() {
+    CUR_BRIGHT=$(</sys/class/backlight/intel_backlight/brightness)
+    NEW_BRIGHT=$(( CUR_BRIGHT - BL_DELTA ))
+
+    # Avoid total darkness
+    [ "${NEW_BRIGHT}" -lt "1" ] && NEW_BRIGHT="1"
+    # Added this because switching to hi-freq backlight
+    # usually results in brightness set to something like 250%
+    [ "${NEW_BRIGHT}" -gt "${BL_CAP}" ] && NEW_BRIGHT="${BL_CAP}"
+
+    echo "${NEW_BRIGHT}" | sudo tee /sys/class/backlight/intel_backlight/brightness
+}
+
+setmode
 case $1 in
     down)
-        xbacklight - 5
-        #herbstclient emit_hook volume
+        down
         ;;
     up)
-        xbacklight + 5
-        #herbstclient emit_hook volume
+        up
         ;;
 esac
 
+notify-send "Brightness: $(( NEW_BRIGHT * 100 / BL_CAP))%"
