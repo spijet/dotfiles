f599bfc8e8619ec8637a9d774a05d2138a5d2aa2
Serge Tkatchouk <sp1j3t@gmail.com>
2016-07-22 10:25:07 +0800

    Install script additions and testing.

--- install.sh
+++ install.sh
@@ -2,45 +2,47 @@
 set -x
 BASEFILE="${0}"
 BASEDIR="$(dirname ${BASEFILE})"
-BCP_DIR="$HOME/.old-dotfiles"
+BCP_DIR="${HOME}/old-dotfiles"
 read foo
-echo "Creating a backup directory..."
-  mkdir $BCP_DIR
+echo "##########################################################################"
+echo "# Creating a backup directory...                                         #"
+echo "##########################################################################"
+mkdir "${BCP_DIR}"
 
 echo "Base dir is ${BASEDIR}"
 
-echo "Installing ZSH stuff..."
-  if [ -f $HOME/.zshrc ]
+echo "##########################################################################"
+echo "# Installing ZSH stuff...                                                #"
+echo "##########################################################################"
+if [ -d "${HOME}/.config/zsh" ]
+then
+    echo "Found older ZSH installation in ${HOME}/.config/zsh. Will move it to ${BCP_DIR}."
+    mv "${HOME}/.config/zsh" "${BCP_DIR}/zsh"
+fi
+ln -s "${BASEDIR}/zsh" "${HOME}/.config/zsh"
+echo "##########################################################################"
+echo "# Installed ZSH config to ${HOME}/.config/zsh. Don't forget to set your  #"
+echo "# ZDOTDIR accordingly, using ~/.zshenv or /etc/zsh/zshenv.               #"
+echo "##########################################################################"
+
+echo "##########################################################################"
+echo "# Installing HLWM symlinks...                                            #"
+echo "##########################################################################"
+  if [ -d "${HOME}/.config/herbstluftwm" ]
     then
-    mv $HOME/.zshrc $BCP_DIR/.zshrc
-    echo "Older .zshrc detected! Will be moved to $BCP_DIR/.zshrc ."
+        echo "##########################################################################"
+        echo "# Older HLWM directory detected!                                         #"
+        echo "# Will be moved to \"${BCP_DIR}/herbstluftwm.\"                          #"
+        echo "##########################################################################"
+        mv "${HOME}/.config/herbstluftwm" "${BCP_DIR}/herbstluftwm"
   fi
-    ln -s $BASEDIR/.zshrc $HOME/.zshrc
+  ln -s "${BASEDIR}/herbstluftwm" "${HOME}/.config/herbstluftwm"
 
-  if [ -d $HOME/.zsh ]
-    then
-    mv $HOME/.zsh $BCP_DIR/.zsh
-    echo "Older .zsh directory detected! Will be moved to ${BCP_DIR}/.zsh ."
-  fi
-  ln -s $BASEDIR/zsh $HOME/.zsh
-  mkdir $HOME/.zsh/plugins
-  git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.zsh/plugins/oh-my-zsh
-  git clone https://github.com/tarruda/zsh-autosuggestions $HOME/.zsh/plugins/autosuggestions
-
-echo "Installing HLWM symlinks..."
-  if [ -d $HOME/.config/herbstluftwm ]
-    then
-    mv $HOME/.config/herbstluftwm $BCP_DIR/herbstluftwm
-    echo "Older HLWM directory detected! Will be moved to $BCP_DIR/herbstluftwm ."
-  fi
-    ln -s $BASEDIR/herbstluftwm $HOME/.config/herbstluftwm
-
-echo "Installing tmux configs..."
-  [ ! -f $HOME/.tmux.conf ] && ln -s $BASEDIR/tmux/tmux.conf $HOME/.tmux.conf
-  [ ! -f $HOME/.tmuxline.conf ] && ln -s $BASEDIR/tmux/tmuxline.conf $HOME/.tmuxline.conf
-
-echo "Installing vim symlinks and cloning Vundle..."
-  [ -e $HOME/.vimrc ] && ln -s ./vim/.vimrc $HOME/.vimrc
-  [ -e $HOME/.vim ] && ln -s ./vim/.vimrc $HOME/.vim
-  git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/Vundle.vim
-
+# echo "Installing tmux configs..."
+#   [ ! -f $HOME/.tmux.conf ] && ln -s $BASEDIR/tmux/tmux.conf $HOME/.tmux.conf
+#   [ ! -f $HOME/.tmuxline.conf ] && ln -s $BASEDIR/tmux/tmuxline.conf $HOME/.tmuxline.conf
+#
+# echo "Installing vim symlinks and cloning Vundle..."
+#   [ -e $HOME/.vimrc ] && ln -s ./vim/.vimrc $HOME/.vimrc
+#   [ -e $HOME/.vim ] && ln -s ./vim/.vimrc $HOME/.vim
+#   git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/Vundle.vim
