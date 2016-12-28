21742e5c2a4e28285bec301f02d2641f2edca946
Serge Tkatchouk <sp1j3t@gmail.com>
2016-12-28 18:31:43 +0800

    Big update of panel script.

--- herbstluftwm/panel.sh
+++ herbstluftwm/panel.sh
@@ -10,17 +10,12 @@ get_volume() { "${CFGDIR}/helpers/volume" get ;}
 
 ## Select AWK implementation:
 if type mawk >/dev/null 2>&1; then
-    uniq_linebuffered() { mawk -W interactive '$0 != l { print ; l=$0 ; fflush(); }' "$@" ;}; else
-    uniq_linebuffered() { awk '$0 != l { print ; l=$0 ; fflush(); }' "$@" ;}
-fi
+    AWK="mawk -W interactive"; else
+    AWK="awk"; fi
 
 monitor="${1:-0}"
 geometry=( $(herbstclient monitor_rect "$monitor") )
-
-if [[ -z "$geometry" ]] ;then
-    echo "Invalid monitor $monitor"
-    exit 1
-fi
+[[ -z "$geometry" ]] && echo "Invalid monitor $monitor" && exit 1
 
 # geometry has the format X Y W H
 x="${geometry[0]}"
@@ -31,94 +26,95 @@ bgcolor="$(herbstclient get frame_border_normal_color)"
 selbg="$(herbstclient get window_border_active_color)"
 selfg='#101010'
 
+fifo_events="/tmp/panelevents.${monitor}"
+[ -e "$fifo_events" ] && rm "$fifo_events"
+mkfifo -m600 "$fifo_events"
+
 # Pad the screen to panel height:
 hc pad "$monitor" "$PANELHEIGHT"
+
+gentagline () {
+    tagline="%{T2}"
+    for i in "${!tags[@]}" ; do
+        case "${tags[i]:0:1}" in
+            '#')
+                tagcolor="%{B$selbg}%{F$selfg}"
+                ;;
+            '+')
+                tagcolor="%{B#9CA668}%{F#141414}"
+                ;;
+            ':')
+                tagcolor="%{B-}%{F#ffffff}"
+                ;;
+            '!')
+                tagcolor="%{B#FF0675}%{F#141414}"
+                ;;
+            *)
+                tagcolor="%{B-}%{F#ababab}"
+                ;;
+        esac
+        tagline+="${tagcolor}%{A:herbstclient chain .-. focus_monitor \"${monitor}\" .-. use \"${tags[i]:1}\":} ${tag_icons[i]} %{A}"
+    done
+}
+
+parsecmd () {
+    cmd="$1"
+    case "${cmd[0]}" in
+        tag*)
+            #echo "resetting tags" >&2
+            IFS=$'\t' read -ra tags <<< "$(hc tag_status $monitor)"
+            gentagline
+            ;;
+        conky)
+            #echo "Getting Conky input" >&2
+            conky="${cmd[1]}"
+            ;;
+        quit_panel)
+            exit
+            ;;
+        reload)
+            pkill -P $$
+            exit
+            ;;
+        focus_changed|window_title_changed)
+            windowtitle="${cmd[2]}"
+            ;;
+        layout)
+            layout="${cmd[1]}"
+            ;;
+        volume)
+            volume="$(get_volume)"
+    esac
+}
 
 # Now I feed the panel via herbstclient hooks.
 # This way I only need one process to be piped to
 #  the parser and lemonbar.
-herbstclient --idle 2>/dev/null | {
-    IFS=$'\t' read -ra tags <<< "$(hc tag_status $monitor)"
-    visible=true
-    conky=""
-    layout="$(skb -1)"
-    windowtitle="Welcome home."
-    volume="$(get_volume)"
-    while true ; do
+herbstclient --idle | $AWK '$0 != l { print ; l=$0 ; fflush(); }' > "$fifo_events" &
 
-        ### Output ###
-        # This part prints panel data based on the _previous_ data handling run,
-        # and then waits for the next event to happen.
-        separator="%{B-}%{F$selbg}|%{F-}"
-        # draw tags
-        echo -n "%{T2}"
-        for i in "${!tags[@]}" ; do
-            case "${tags[i]:0:1}" in
-                '#')
-                    echo -n "%{B$selbg}%{F$selfg}"
-                    ;;
-                '+')
-                    echo -n "%{B#9CA668}%{F#141414}"
-                    ;;
-                ':')
-                    echo -n "%{B-}%{F#ffffff}"
-                    ;;
-                '!')
-                    echo -n "%{B#FF0675}%{F#141414}"
-                    ;;
-                *)
-                    echo -n "%{B-}%{F#ababab}"
-                    ;;
-            esac
-            echo -n "%{A:herbstclient chain .-. focus_monitor \"${monitor}\" .-. use \"${tags[i]:1}\":} ${tag_icons[i]} %{A}"
-        done
-        echo -n "%{T1}${separator} "
-        echo -n "${windowtitle//%{/% {}"
-        echo -n "%{r} $layout $separator $volume $separator "
-        echo -n "$conky "
-        echo
-
-        ### Data handling ###
-        # This part handles the events generated in the event loop, and sets
-        # internal variables based on them. The event and its arguments are
-        # read into the array cmd, then action is taken depending on the event
-        # name.
-        # "Special" events (quit_panel/togglehidepanel/reload) are also handled
-        # here.
-        # wait for next event
-        IFS=$'\t' read -ra cmd || break
-        # find out event origin
-        case "${cmd[0]}" in
-            tag*)
-                #echo "resetting tags" >&2
-                IFS=$'\t' read -ra tags <<< "$(hc tag_status $monitor)"
-                ;;
-            conky)
-                #echo "Getting Conky input" >&2
-                conky="${cmd[@]:1}"
-                ;;
-            quit_panel)
-                exit
-                ;;
-            reload)
-                pkill -P $$
-                exit
-                ;;
-            focus_changed|window_title_changed)
-                windowtitle="${cmd[@]:2}"
-                ;;
-            layout)
-                layout="${cmd[@]:1}"
-                ;;
-            volume)
-                volume="$(get_volume)"
-        esac
-    done
-
-    ### bar ###
-    # After the data is gathered and processed, the output of the previous block
-    # gets piped to (lemon)bar.
-
-} 2>/dev/null | uniq_linebuffered | lemonbar -g "${panel_width}x${PANELHEIGHT}+${x}+${y}" \
-                          -f "${TEXTFONT}" -f "${GLYPHFONT}" \
-                          -B "$bgcolor" -F '#efefef'
+## Process stuff:
+IFS=$'\t' read -ra tags <<< "$(hc tag_status $monitor)"
+gentagline
+conky=""
+layout="$(skb -1)"
+windowtitle="Welcome home."
+volume="$(get_volume)"
+IFS=$'\t'
+separator="%{B-}%{F$selbg}|%{F-}"
+while read -ra cmd; do
+    ### Data handling ###
+    # This part handles the events generated in the event loop, and sets
+    # internal variables based on them. The event and its arguments are
+    # read into the array cmd, then action is taken depending on the event
+    # name.
+    # "Special" events (quit_panel/togglehidepanel/reload) are also handled
+    # here.
+    parsecmd "${cmd}"
+    # Draw tags and window title:
+    printf "%s" "%{T2}${tagline}%{T1}${separator} ${windowtitle//%{/% {}"
+    # Draw info stuff on the right side of the panel and finish the line:
+    printf "%s\n" "%{r} $layout $separator $volume $separator $conky "
+done < "$fifo_events" |\
+lemonbar -g "${panel_width}x${PANELHEIGHT}+${x}+${y}" \
+         -f "${TEXTFONT}" -f "${GLYPHFONT}" \
+         -B "$bgcolor" -F '#efefef'
